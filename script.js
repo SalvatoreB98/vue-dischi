eed3d93ce72b75c5a6b0bfa79b8ad2a7bda1f424
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-12 15:07:08 +0200

    Esercizio OK

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ window.addEventListener("load", () => {
         el: '#vueContainer',
         data: {
             albums: [],
+            genres: [],
+            selected : '',
+            filteredAlbums : []
         },
         methods: {
-
+            onChange(){
+               this.filteredAlbums = this.albums.filter((element)=>{ return this.selected == element.genre || this.selected == 'all' })
+            },
+            selectAll(){
+                this.filteredAlbums = resp.data.response;
+            }
         },
         mounted() {
             axios.get('https://flynn.boolean.careers/exercises/api/array/music')
                 .then((resp) => {
                     this.albums = resp.data.response;
+                    this.filteredAlbums = resp.data.response
+                    
+                    this.albums.forEach(element => {
+                        if(!this.genres.includes(element.genre))
+                        {
+                           this.genres.push(element.genre);
+                        }
+                    });
                 })
+           
         }
     })
 })
